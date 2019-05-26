60cd192340834a405dade08041f5d8f130646b10
Kazuki Tobita <kazukiti201@gmail.com>
2019-05-26 11:15:08 +0900

    Modify shock.py

--- index.php
+++ index.php
@@ -3,5 +3,5 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json; charset=utf-8');
 
-exec('./shock.py');
+exec('python3 shock.py');
 echo json_encode([ 'status' => 'OK' ]);
--- shock.py
+++ shock.py
@@ -5,12 +5,12 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.OUT)
-try:
-        while True:
-                GPIO.output(24, True)
-                time.sleep(0.4)
-                GPIO.output(24, False)
-                time.sleep(0.4)
+
+try
+    GPIO.output(24, True)
+    time.sleep(0.4)
+    GPIO.output(24, False)
+    time.sleep(0.4)
 
 except KeyboardInterrupt:
         pass
