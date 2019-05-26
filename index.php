029ec343e3f0d8ac1699ff108ab94751bdb222a7
Kazuki Tobita <kazukiti201@gmail.com>
2019-05-26 10:44:06 +0900

    Add CORS

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
-header("Content-Type: application/json; charset=utf-8");
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json; charset=utf-8');
 
 // exec('./shock.py');
 echo json_encode([ 'status' => 'OK' ]);
